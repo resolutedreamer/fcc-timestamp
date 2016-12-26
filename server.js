9c9945a3ca38fcde688c0967b2973d6f2c599a36
Anthony Nguyen <resolutedreamer@users.noreply.github.com>
2016-12-26 13:03:19 -0800

    🍯💐 Updated with Gomix

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
+// server.js
+// where your node app starts
+
+// init project
 var express = require('express');
 var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
 
 app.get("/*", function (req, res) {
   var params = req.params;
@@ -24,8 +44,4 @@ app.get("/*", function (req, res) {
   else {
     res.json(result)
   }
-});
-
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
 });
