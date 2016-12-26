083e4bf58aef427299cbfbdb6dc59ac139049892
Anthony Nguyen <anthony.c.nguyen@ucla.edu>
2016-12-26 05:35:12 +0000

    Completed API

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("/*", function (req, res) {
+  var params = req.params;
+  var input = params['0'];
+  console.log(input);
+  var myDate = new Date(input);
+  var result = {}
+  result["unix"] = null;
+  result["natural"] = null;
+  console.log(myDate);
+  if (myDate.toString() !== "Invalid Date") {
+    var locale = "en-us"
+    var naturalDate = ""
+    var month = myDate.toLocaleString(locale, { month: "long" });
+    var year = myDate.getFullYear();
+    var day = myDate.getDate();
+    naturalDate = month + " " + day + ", " + year;
+    result["unix"] = myDate.getTime();
+    result["natural"] = naturalDate;
+    res.json(result)
+  }
+  else {
+    res.json(result)
+  }
 });
 
 app.listen(8080, function () {
